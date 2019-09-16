c0ed0b84287070b6032d20e3dd7ccd1f9fb8211d
Lucas Henrique <lucasheriques@gmail.com>
2019-09-15 21:20:36 -0300

    Changing data

--- SampleService.cs
+++ SampleService.cs
@@ -12,16 +12,28 @@ public class SampleService : ISampleService
         teachers = new Dictionary<int, Teacher>();
         teacher = new Teacher();
         teacher.Id = 1;
-        teacher.Name = "Lucas";
+        teacher.Name = "Lesandro Ponciano";
+        teacher.Description = "Ótimo professor e orientador!";
+        teacher.Disciplines = "Interação Humano Computador";
+        teacher.University = "PUC Minas";
+        teacher.Birthday = "22/01/1985";
         teachers.Add(teacher.Id, teacher);
         Teacher teacher2 = new Teacher();
         teacher2.Id = 2;
-        teacher2.Name = "Iara";
-        teacher2.Description = "Great teacher!";
+        teacher2.Name = "Maria Augusta";
+        teacher2.Description = "Ótimo professor e orientador!";
+        teacher2.Disciplines = "Interação Humano Computador";
         teacher2.University = "PUC Minas";
-        teacher2.Birthday = "18/10/1996";
-        teacher2.Disciplines = "Mobile";
+        teacher2.Birthday = "22/01/1985"; ;
         teachers.Add(teacher2.Id, teacher2);
+        Teacher teacher3 = new Teacher();
+        teacher3.Name = "Maria Werneck";
+        teacher3.Id = 2;
+        teacher3.Description = "Ótimo professor e orientador!";
+        teacher3.Disciplines = "Interação Humano Computador";
+        teacher3.University = "PUC Minas";
+        teacher3.Birthday = "22/01/1985"; ;
+        teachers.Add(teacher3.Id, teacher3);
 
     }
     public string Test(string s)
